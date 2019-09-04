009b7d301cc800ba0a67c82b5de9ab63f65aed51
labaran1 <adlabaran@gmail.com>
2019-09-04 14:10:12 -0400

    mobile view

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -421,7 +421,7 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 modules:true,
-                localIdentName:'[name]__[local]__[hash:base64:5]',
+                localIdentName:'[name]__[local]__[hash:base64:5]'
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
-import Toolbar from '../../component/Navigation/Toolbar/Toolbar';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) =>(
-<Aux>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-<Toolbar/>
- {/* <div> Toolbar, SideDrawer, Backdrop</div> */}
-<main className={classes.Content}>
-    {props.children}
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
 
-</main>
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
 
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
-</Aux>
-)
-
-
-
-
-// Layout.propTypes = {
-
-// }
-
-export default Layout
-
+export default Layout;
