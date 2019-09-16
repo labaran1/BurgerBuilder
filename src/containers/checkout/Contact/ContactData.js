b95819a262f3619c484d56de727f14861ffc5e83
labaran1 <adlabaran@gmail.com>
2019-09-15 21:31:30 -0400

    custome validation

--- src/containers/checkout/Contact/ContactData.js
+++ src/containers/checkout/Contact/ContactData.js
@@ -16,7 +16,12 @@ import Input from '../../../component/UI/Input/Input';
               type: 'text',
               placeholder: 'Your Name'
             },
-            value: ''
+            value: '',
+            validation: {
+              required:true
+            },
+            valid: false
+
           },
         street: {
           elementType: 'input',
@@ -24,7 +29,12 @@ import Input from '../../../component/UI/Input/Input';
             type: 'text',
             placeholder: 'Street'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
+
         },
         zipCode: {
           elementType: 'input',
@@ -32,7 +42,13 @@ import Input from '../../../component/UI/Input/Input';
             type: 'text',
             placeholder: "ZIP CODE"
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true,
+            minLength: 5, 
+            maxLength: 5
+          },
+          valid: false
         },
         country: {
           elementType: 'input',
@@ -40,7 +56,11 @@ import Input from '../../../component/UI/Input/Input';
             type: 'text',
             placeholder: 'Country'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         email: {
           elementType: 'input',
@@ -48,7 +68,11 @@ import Input from '../../../component/UI/Input/Input';
             type: 'email',
             placeholder: 'Your E-Mail'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         deliveryMethod: {
           elementType: 'select',
@@ -98,6 +122,21 @@ orderHandler = (event)=>{
     });
 
 }
+checkValidity(value, rules){
+ let isValid = true;
+
+ if(rules.required){
+   isValid = value.trim() !== '' && isValid;
+ }
+ if(rules.minLength){
+   isValid = value.length >= rules.minLength && isValid
+ }
+  if (rules.macLength) {
+    isValid = value.length <= rules.maxLength && isValid
+  }
+
+ return isValid;
+}
 
 inputChangeHandler = (event, inputIdentifier)=> {
 // console.log(event.target.value)
@@ -108,7 +147,9 @@ const updatedFormElement = {
   ...updatedOrderForm[inputIdentifier]
 };
 updatedFormElement.value = event.target.value;
+updatedFormElement.valid =this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
 updatedOrderForm[inputIdentifier]= updatedFormElement;
+console.log(updatedFormElement)
 this.setState({orderForm:updatedOrderForm})
 
 }
